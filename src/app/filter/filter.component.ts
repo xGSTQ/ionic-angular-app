eafcc06b9f704b62bd1af7044e56a5effb6a04e5
James <xgodsavethequeen@gmail.com>
2022-12-29 17:57:33 +0000

    Get filters working on locations tab

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {LOCATIONS} from '../../assets/locations';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
-  // pipes: [FilterPipe]
 })
 export class FilterComponent implements OnInit {
+  @Input() locations: Array<any>;
+  @Output() itemsChange = new EventEmitter<any[]>();
 
   selectedCounty: string;
   selectedBestTime: string;
@@ -17,71 +18,73 @@ export class FilterComponent implements OnInit {
   bestTimeOptions = ['Sunrise', 'Sunset', 'Midday'];
   seasonOptions = ['Any', 'Spring', 'Autumn', 'Winter'];
 
-  locations = LOCATIONS;
-
-  // locations = [
-  //   {
-  //     name: 'location one name',
-  //     county: 'Staffordshire',
-  //     bestTime: 'Sunrise',
-  //     season: 'Autumn',
-  //   },
-  //   {
-  //     name: 'location two name',
-  //     county: 'Derbyshire',
-  //     bestTime: 'Sunrise',
-  //     season: 'Autumn',
-  //   },
-  //   {
-  //     name: 'location three name',
-  //     county: 'Cheshire',
-  //     bestTime: 'Sunrise',
-  //     season: 'Winter',
-  //   },
-  //   {
-  //     name: 'location four name',
-  //     county: 'Cheshire',
-  //     bestTime: 'Sunset',
-  //     season: 'Winter',
-  //   },
-  //   {
-  //     name: 'location five name',
-  //     county: 'Derbyshire',
-  //     bestTime: 'Sunset',
-  //     season: 'Spring',
-  //   },
-  //   {
-  //     name: 'location six name',
-  //     county: 'Derbyshire',
-  //     bestTime: 'Sunrise',
-  //     season: 'Spring',
-  //   },
-  //   {
-  //     name: 'location seven name',
-  //     county: 'Staffordshire',
-  //     bestTime: 'Anytime',
-  //     season: 'Any',
-  //   },
-  // ];
-
   get selectedValues() {
     return {
       county: this.selectedCounty,
       bestTime: this.selectedBestTime,
       season: this.selectedSeason,
     };
+
+  }
+
+  apply() {
+    this.locations = LOCATIONS;
+
+    // const keysExact  = ['county', 'season', 'bestTime'];
+    // const valuesExact  = ['Staffordshire', 'Spring', 'Sunrise']; // Grindon Moor
+
+    // Return the keys of the dropdowns selected for the filter
+    let keysExact = [];
+    if (this.selectedValues.county) {
+      keysExact.push('county');
+    }
+    if(this.selectedValues.season) {
+      keysExact.push('season');
+    }
+    if(this.selectedValues.bestTime) {
+      keysExact.push('bestTime');
+    }
+
+    // Return the values from the dropdowns
+    const valuesExact  = [this.selectedValues.county, this.selectedValues.season, this.selectedValues.bestTime];
+
+    // If selecting 'All' in the dropdowns
+    if(this.selectedValues.county === 'All') {
+      valuesExact.splice(0,1,'Staffordshire', 'Derbyshire', 'Cheshire', 'Greater Manchester', 'West Yorkshire', 'South Yorkshire');
+    }
+
+    if(this.selectedValues.season === 'All') {
+      valuesExact.splice(1,1, 'Any', 'Spring', 'Autumn', 'Winter')
+    }
+
+    if(this.selectedValues.bestTime === 'All') {
+      valuesExact.splice(2,1, 'Sunrise', 'Sunset', 'Midday')
+    }
+
+    // console.log(this.locations)
+    // console.log(keysExact)
+    // console.log(valuesExact)
+
+    const resultExact = this.locations.filter((item) =>
+      keysExact.every( (key) =>
+        valuesExact.some((val) => item[key].includes(val))
+      )
+    );
+
+    // console.log(resultExact)
+
+    this.itemsChange.emit(resultExact);
   }
 
   resetAll() {
     this.selectedCounty = null;
     this.selectedBestTime = null;
     this.selectedSeason = null;
-    this.locations = this.locations;
+    this.locations = LOCATIONS;
+    this.itemsChange.emit(this.locations);
   }
 
-  constructor() {}
 
   ngOnInit() {
-
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,31 +10,19 @@ import {LOCATIONS} from '../../assets/locations';
 })
 export class Tab1Page implements OnInit {
 
-  menuType: string = 'reveal';
+  menuType: string = 'push';
   @ViewChild(IonContent) content: IonContent;
   locations = LOCATIONS;
   public searchText = '';
   isLoading = true;
   logs: string[] = [];
+
   public selectedCounty = 'All';
   public selectedSeason = 'All';
-
-  // selectedCounty: string;
   selectedBestTime: string;
-  // selectedSeason: string;
 
-  currentFood = undefined;
 
-  // public filter = [];
-
-  filter = {
-    county: ["Staffordshire"],
-    season: ["Autumn"],
-    difficulty: 1
-  };
-
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -42,142 +30,6 @@ export class Tab1Page implements OnInit {
     setTimeout(() => {
       this.isLoading = false;
     }, 500);
-
-  }
-
-
-  // Filter by County Test
-  pushLog(msg) {
-    this.logs.unshift(msg);
-  }
-
-  get selectedValues() {
-    return {
-      county: this.selectedCounty,
-      bestTime: this.selectedBestTime,
-      season: this.selectedSeason,
-    };
-  }
-
-
-  // Test a filter on multiple dropdowns
-  // test function
-  testFilter = (filter) => {
-    console.log('------ with filter: ', filter)
-
-    let thefilter = Object.entries(filter);
-    const result = this.locations.filter(item => {
-      let c = 0;
-      // can only return true if each condition passes
-      thefilter.length === 0 || thefilter
-        .map(([key, val]) => {
-          if (!Array.isArray(val)) val = [val]
-          // @ts-ignore
-          c += val.filter(v => {
-            return item[key] === v;
-          }).length;
-        })
-      return c >= thefilter.length
-    })
-
-    this.locations = result;
-    console.log(this.locations)
-    console.log('Found ' + result.length + ' results: ', result);
-  }
-
-// testing
-
-
-  /* ---------- */
-
-  // filter = {
-  //   bike: "Mountain"
-  // }
-  // testFilter(filter)
-
-  /* ---------- */
-
-  // filter = {
-  //   bike: "Mountain",
-  //   country: ["FR", "USA", "GB"]
-  // }
-  // testFilter(filter)
-
-  /* ---------- */
-
-  // filter = {}
-  // testFilter(filter)
-
-  // end test filter
-
-
-// test
-//   captureQuery(e) {
-//     let query = e.detail.value
-//     return  this.locations.filter(this.search, query);
-//   }
-//   search(user) {
-//     return Object.keys(this).every((key) => this.locations[key] === this[key]);
-//   }
-// end test
-
-  // Filter by County
-  handleChangeCounty(e) {
-    this.selectedCounty = e.detail.value || 'All';
-    // this.pushLog('ionChange fired with value: ' + e.detail.value);
-
-    if (e.detail.value === 'all') {
-      this.locations = LOCATIONS;
-    } else {
-      this.filterByCounty(e.detail.value);
-    }
-    // console.log(this.locations)
-  }
-
-// Filter by Season
-  handleChangeSeason(e) {
-    // this.selectedSeason = e.detail.value || 'All';
-    this.pushLog('ionChange fired with value: ' + e.detail.value);
-
-    this.testFilter({season: [e.detail.value]})
-  }
-
-
-  scrollToTop() {
-    // Passing a duration to the method makes it so the scroll slowly
-    // goes to the top instead of instantly
-    this.content.scrollToTop(500);
-  }
-
-  // newTest = [];
-  // Iterate through the locations object to get all the name
-  // locationsByName(): void {
-  //   let i = 0;
-  //   while (i < this.locations.length) {
-  //     this.newTest.push(this.locations[i].name.toLowerCase());
-  //     i++;
-  //   }
-  // }
-
-
-  // Filtering the locations by county
-  filterByCounty(county: any): void {
-    // this.locations = LOCATIONS;
-    const filterBy = county;
-    console.log(filterBy)
-    this.locations = this.locations.filter((i) => filterBy.includes(i.county));
-    // this.filter = this.locations
-    // this.searchText = county
-  }
-
-  // Filtering the locations by Season
-  filterBySeason(query: any): void {
-    // const filterBy = season;
-    console.log(query)
-    // this.filter = {season: [query]};
-    this.testFilter(this.filter);
-    // this.locations = this.locations.filter((i) => filterBy.includes(i.season));
-    // this.filter = this.locations
   }
 
 }
